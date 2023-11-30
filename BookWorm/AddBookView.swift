89b505844cbd84abeef5e57d6d7d8b94f35aeeaa
Tim Mitra <tim@it-guy.com>
2023-11-30 01:41:56 -0500

    ready to add a book

---
+++ BookWorm/AddBookView.swift
@@ -0,0 +1,18 @@
+//
+//  AddBookView.swift
+//  BookWorm
+//
+//  Created by Tim Mitra on 2023-11-30.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddBookView()
+}
