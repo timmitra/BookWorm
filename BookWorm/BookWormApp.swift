da99aa285095665875afd1865d7e00eea6b0d30f
Tim Mitra <tim@it-guy.com>
2023-11-30 00:32:13 -0500

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Tim Mitra on 2023-11-30.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookWorm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BookWorm
+//
+//  Created by Tim Mitra on 2023-11-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
