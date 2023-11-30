bb34243d9a8b43649098732a1bf1fb469b5e36d6
Tim Mitra <tim@it-guy.com>
2023-11-30 01:42:11 -0500

    add book

--- BookWorm/ContentView.swift
+++ BookWorm/ContentView.swift
@@ -6,17 +6,30 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  @Environment(\.modelContext) var modelContext
+  @Query var books: [Book]
+  
+  @State private var showingAddScreen = false
+  
+  var body: some View {
+    NavigationStack {
+      Text("Count: \(books.count)")
+        .navigationTitle("Bookworm")
+        .toolbar {
+          ToolbarItem(placement: .topBarTrailing) {
+            Button("Add Book", systemImage: "plus") {
+              showingAddScreen.toggle()
+            }
+          }
         }
-        .padding()
+        .sheet(isPresented: $showingAddScreen) {
+          AddBookView()
+        }
     }
+  }
 }
 
 #Preview {
